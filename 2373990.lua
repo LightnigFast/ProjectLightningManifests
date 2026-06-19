84a388a4b1e5ff7f66aea1c3f747fb0baedcc836
LightningFast <bydraxx4k129@gmail.com>
2026-06-19 10:18:38 +0200

    Create 2373990.lua

---
+++ 2373990.lua
@@ -0,0 +1,8 @@
+addappid(2373990) -- Solo Leveling: ARISE OVERDRIVE
+addappid(2373991, 1, "5a97586d39bdb2a503376a75e3cae7d1f7e2de1bcb9ced523a73b21ca79de413") -- Depot 2373991
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(4240370) -- Solo Leveling:ARISE OVERDRIVE - Christmas Costume Gift Pack
+addappid(4710940) -- Solo Leveling:ARISE OVERDRIVE - Jeju Island Raid Special Pack
+addappid(3638570) -- Solo LevelingARISE OVERDRIVE - Deluxe Edition
+
+-- Made with love by LightningFast⚡💜
