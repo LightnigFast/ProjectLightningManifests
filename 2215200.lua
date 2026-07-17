d72268a7d4bf3d401060f193f7754ea2b5dd2ca4
LightningFast <bydraxx4k129@gmail.com>
2026-07-17 21:59:52 +0200

    Create 2215200.lua

---
+++ 2215200.lua
@@ -0,0 +1,9 @@
+
+addappid(2215200) -- LEGO® Batman™: Legacy of the Dark Knight
+addappid(2215201, 1, "5d2bafc1381c29ebabb37bbab50e496548be24f6bf5d511e8d471b99d60cfbf3") -- Depot 2215201
+setManifestid(2215201,"7325378332099278335")
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(4168610) -- The Dark Knight Returns Suit
+
+-- Made with love by LightningFast⚡💜
